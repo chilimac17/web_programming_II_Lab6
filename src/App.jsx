5d09711e16991a15455a4f709081e0e96f6af9d6
Michael Chillemi <michaelchillemi17@gmail.com>
2026-03-30 23:58:25 -0400

    cleaning up page

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,32 @@ import { Routes, Link, Route } from "react-router-dom";
 import './App.css'
 
 function App() {
-  //<Link to="/pokemon/1">Pokemon</Link>
-     // <Link to="/pokemonList">Pokemon List</Link>
+  
      //        <Route path="/pokemon/:id" element={<Pokemon />} />
 
-
+    // <Route path="/pokemonList" element={<PokemonList />} />
+    //    <Route path="/abilities/" element={<Abilities />} />
+    //    <Route path="/types/" element={<Types />} />
   return (
     <div>
       <h1>Pokemon App JSX</h1>
       <Link to="/">Home</Link>
       <br />
-      <Link to="/pokemon/">Pokemon ID Brows</Link>
+      <Link to="/pokemon/">Pokemon ID</Link>
+       <br />
+      <Link to="/pokemonList">Pokemon</Link>
+       <br />
+      <Link to="/abilities/">Abilities </Link>
+       <br />
+      <Link to="/types/">Types </Link>
       
       <br />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pokemon/:id" element={<Pokemon />} />
+        {/* <Route path="/pokemonList" element={<PokemonList />} /> */}
+        {/* <Route path="/abilities/" element={<Abilities />} /> */}
+        {/* <Route path="/types/" element={<Types />} /> */}
       </Routes>
     </div>
   )
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import {Card, CardContent, CardActionArea, CardMedia, Typography, CardHeader, Grid} from "@mui/material";
+import { Link } from "react-router-dom";
+
+
 //import Pokemon from "./components/Pokemon";
 //import pokemonData from "../data/pokemonData";
 //<pokemonData pokemonId={1} />
      //       <pokemonData pokemonId={2} />
         //    <pokemonData pokemonId={3} />
 function PokemonList(props) {
+
     return (
-        <div className="pokemonList">
+        <Grid container spacing={2}>
             
-
-            <p>This is the pokemon list page of our React application.</p>
-
-        </div>
+        </Grid>
     );
 }
